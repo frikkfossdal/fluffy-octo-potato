bef6953a31a170cfdba07073308b7c6c4d01fed3
Frikk H Fossdal <frikkfossdal@gmail.com>
2018-04-27 18:57:01 +0200

    new geo and updated positioning algorithm

--- src/Model.cpp
+++ src/Model.cpp
@@ -4,12 +4,14 @@
 //
 //  Created by Frikk Fossdal on 19.02.2018.
 //
+//TODO:
+//1. Run this (?) class as a thread so it doesnt stall the main program.
+//2. Slic
 
 #include "Model.hpp"
 
 Model::Model(){
-    threeDeeFile.loadModel("Test1.stl");
-    fixPosition();
+    threeDeeFile.loadModel("stress.stl");
     setup();
 }
 void Model::setup(){
@@ -21,12 +23,13 @@ void Model::setup(){
     parameters.add(drawContours.set("drawContours",false));
     parameters.add(slice.set("Start slicing", false));
     parameters.add(modelPosition.set("position", ofVec3f(0,0,0), ofVec3f(0,0,0), ofVec3f(400,400,50)));
-    parameters.add(scl.set("Scale", 1, 0, 10));
+    parameters.add(scl.set("Scale", 1, 0, 60));
     sliceFinish = false;
 }
 void Model::update(){
     threeDeeFile.setPosition(modelPosition->x, modelPosition->y, modelPosition->z);
     threeDeeFile.setScale(scl,scl,scl);
+    threeDeeFile.update();
     
     //slice_button setup
     //add logic that destroys the existing sliced object. 
@@ -41,7 +44,6 @@ void Model::showModel(){
     if(drawWireFrame == true){
         threeDeeFile.drawWireframe();
     }
-    ofDrawRectangle(0, 0, 300, 300);
     if(layers.size() > 10)
     {
         layers[layerIndex].show();
@@ -68,14 +70,26 @@ void Model::incSlice(){
 
 void Model::buildTriangles(){
     //create vector containing all faces of mesh
+    //IMPORTANT: include modelMatrix transformation
     int meshIndex = 0;
     ofMesh mesh = threeDeeFile.getMesh(meshIndex);
+    
+    ofMatrix4x4 modelMatrix = threeDeeFile.getModelMatrix();
+    ofMatrix4x4 meshMatrix = threeDeeFile.getMeshHelper(meshIndex).matrix;
+    ofMatrix4x4 concatMatrix;
+    concatMatrix.preMult(modelMatrix);
+    concatMatrix.preMult(meshMatrix);
+    
+    for(int i = 0; i < mesh.getNumVertices(); i++){
+        ofVec3f & vert = mesh.getVertices()[i];
+        vert.set(concatMatrix.preMult(vert));
+        mesh.setVertex(i, vert);
+    }
     
     std::vector<ofMeshFace> faces = mesh.getUniqueFaces();
     //loop through the faces and create triangle objects
     for(auto f = faces.begin(); f != faces.end(); f++){
-        Triangles newTriangle(f->getVertex(0).operator+(threeDeeFile.getPosition()),f->getVertex(1).operator+(threeDeeFile.getPosition()), f->getVertex(2).operator+(threeDeeFile.getPosition()));
-        
+        Triangles newTriangle(f->getVertex(0),f->getVertex(1), f->getVertex(2));
         triangleList.push_back(newTriangle);
     }
     //sort triangleList in terms of z-height
@@ -112,11 +126,9 @@ void Model::createLayers(){
     sliceFinish = true;
 }
 void Model::fixPosition(){
-    //You have to errors.
-    //1. The calculated triangles seems to be mirrored
-    //2. The position of the calculated triangles is wrong
-    ofVec3f sceneMax = threeDeeFile.getSceneMax();
-    threeDeeFile.setScaleNormalization(false);
-    threeDeeFile.enableNormals();
+    //This function should:
+    //1. Set the lowest z-value of 3D model to the slicing surface --> reposition the model
+    //2. Update the mesh used by triangle-algorithm with the data from the assimp model-matrix.
+    //3. (?) Calculate and draw a binding box around the model.
 }
 
